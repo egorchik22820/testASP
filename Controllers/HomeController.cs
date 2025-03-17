f27b27d55a99e8a3ad458b7a0d816ee739291074
10240013 <10240013@D205PC29>
2025-03-17 11:24:33 +0300

    dfgdgdgf

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,7 +15,36 @@ namespace test.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            var products = new List<Product>
+        {
+            new Product
+            {
+                ID = 1,
+                ProdName = "Ноутбук",
+                ProdLastName = "Мощный ноутбук для работы",
+                ProdDiscription = "Мощный ноутбук с отличной производительностью.",
+                ProdPrice = 50000,
+                Images = "/images/laptop.jpg", // Убедитесь, что изображение существует по указанному пути
+                Status = new Status(), // Замените на ваши актуальные значения для Status и других свойств
+                TypesOfDevice = new TypesOfDevice(),
+                Manufacturer = new Manufacturer() // Замените на актуальное значение
+            },
+            new Product
+            {
+                ID = 2,
+                ProdName = "Смартфон",
+                ProdLastName = "Современный смартфон",
+                ProdDiscription = "Смартфон с отличными функциями.",
+                ProdPrice = 30000,
+                Images = "/images/smartphone.jpg", // Убедитесь, что изображение существует по указанному пути
+                Status = new Status(), // Замените на ваши актуальные значения для Status и других свойств
+                TypesOfDevice = new TypesOfDevice(),
+                Manufacturer = new Manufacturer() // Замените на актуальное значение
+            },
+            // Добавьте больше продуктов здесь...
+        };
+
+            return View(products);
         }
 
         public IActionResult Privacy()
@@ -28,5 +57,7 @@ namespace test.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+
     }
 }
